45b03837e61565b3581c7b998f2b264db0c4e037
Gary Reynolds <gary@touch.asn.au>
2022-12-28 12:32:40 +1100

    Add eligibility.Player model

--- eligibility/admin.py
+++ eligibility/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from eligibility.models import Country
+from eligibility.models import Country, Player
 
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ("name", "iso3166a3")
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ("name", "date_of_birth", "country_of_birth")
--- eligibility/models.py
+++ eligibility/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 
@@ -8,6 +10,45 @@ class Country(models.Model):
     class Meta:
         verbose_name_plural = "countries"
         ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    country_of_birth = models.ForeignKey(
+        Country,
+        blank=True,
+        null=True,
+        help_text="If not one of these countries, leave blank and type below.",
+        on_delete=models.PROTECT,
+        related_name="players_born",
+    )
+    country_of_birth_other = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        default=None,
+        help_text="Only valid if not one of the countries listed above.",
+    )
+    residence = models.ForeignKey(
+        Country,
+        blank=True,
+        null=True,
+        help_text="If not one of these countries, leave blank and type below.",
+        on_delete=models.PROTECT,
+        related_name="players_resident",
+    )
+    residence_other = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        default=None,
+        help_text="Only valid if not one of the countries listed above.",
+    )
 
     def __str__(self):
         return self.name
