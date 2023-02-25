2e7f4b43cb9d595f9d53b4ca9f42fd9b84ea9cd9
Gary Reynolds <gary@touch.asn.au>
2023-02-26 08:30:08 +1100

    add sentry-sdk

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path("", include("eligibility.urls")),
     path("", include("social_django.urls", namespace="social")),
@@ -8,4 +11,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("userprofile.urls")),
+    path("sentry-debug", trigger_error),
 ]
